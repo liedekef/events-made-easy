1e230ee7a637d5918650bb25d7bff93baf3ca6ac
Franky Van Liedekerke <franky@e-dynamics.be>
2023-01-16 16:22:51 +0100

    new langs script

--- langs/gettextize.sh
+++ langs/gettextize.sh
@@ -1,23 +1,43 @@
-# First copy /var/www/html/wordpress/wp-includes/pomo/ into pot_gen
+#!/bin/bash
 
+# First copy /var/www/html/wordpress/wp-includes/pomo/ into pot_gen
 # Now you can generate the pot file:
 
 cd pot_gen
 # path to plugin
-plugin_dir="../../"
+plugin_dir=`realpath ../../`
+lang_dir="$plugin_dir/langs"
 # excludes langs/pot_gen,plugin-update-checker are set fixed in makepot.php
-php makepot.php wp-plugin $plugin_dir "$plugin_dir/langs/events-made-easy.pot"
-cd "$plugin_dir/langs"
+mv "$lang_dir/events-made-easy.pot" "$lang_dir/events-made-easy-orig.pot"
+php makepot.php wp-plugin $plugin_dir "$lang_dir/events-made-easy.pot"
+diff -q -d -I '^#' -I '^\"' "$lang_dir/events-made-easy-orig.pot" "$lang_dir/events-made-easy.pot" >/dev/null
+if [ $? -gt 0 ]; then
+	# the new pot file differs from the old, so let's regenerate all
+	echo "new pot file differs from old, regenerating"
+	cd "$lang_dir"
+	for i in `ls *po`; do
+		j=`echo "${i%.*}"`
+		# first remove old location comments
+		grep -v '^# File:' $i > "tmp.po"
+		# now merge
+		echo "==> Merging pot into new $i"
+		msgmerge --strict -o "$i" "tmp.po" events-made-easy.pot
+		rm "tmp.po"
+	done
+else
+	mv "$lang_dir/events-made-easy-orig.pot" "$lang_dir/events-made-easy.pot"
+fi
+
+# regenerate mo files if po file is newer
+# doing this "later" also covers the case where someone provided a new pot file
+# via git/mail
+cd "$lang_dir"
 for i in `ls *po`; do
-   j=`echo "${i%.*}"`
-   # first remove old location comments
-   grep -v '^# File:' $i > "$j-new.po"
-   mv "$j-new.po" "$j.po"
-   msgmerge --strict -o "$j-new.po" "$j.po" events-made-easy.pot
-   echo "==> Compiling strings for $i"
-   msgfmt --strict -c -v -o "$j-new.mo" "$j-new.po"
-   mv "$j-new.po" "$j.po"
-   mv "$j-new.mo" "$j.mo"
+        j=`echo "${i%.*}"`
+        if [ "$i" -nt "$j.mo" ]; then
+		echo "==> PO newer than MO, recompiling strings for $i"
+		msgfmt --strict -c -v -o "$j.mo" "$i"
+        fi
 done
 
 echo

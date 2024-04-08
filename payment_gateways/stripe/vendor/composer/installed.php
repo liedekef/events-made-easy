16dd8ae3aac115ad74b8370f97324e2c66ddde17
Franky Van Liedekerke <franky@e-dynamics.be>
2024-04-08 12:54:35 +0200

    stripe dep updates

--- payment_gateways/stripe/vendor/composer/installed.php
+++ payment_gateways/stripe/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'stripe/stripe-php',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c4759f0a8661f2e4a3a0ccd67e90d37af34572b7',
+        'reference' => '16d4d625b0c755bb8b15779801e5eea4f3fe99fd',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'stripe/stripe-php' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c4759f0a8661f2e4a3a0ccd67e90d37af34572b7',
+            'reference' => '16d4d625b0c755bb8b15779801e5eea4f3fe99fd',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),

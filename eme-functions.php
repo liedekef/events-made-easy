c9030a5df0936ffb3c92889e5a1ec0c257d483ec
Franky Van Liedekerke <franky@e-dynamics.be>
2024-05-25 20:02:46 +0200

    limit numbers of admins to 1, we only need 1 anyway

--- eme-functions.php
+++ eme-functions.php
@@ -766,7 +766,7 @@ function eme_get_contact( $contact_id = 0 ) {
 		}
 		// still nothing ? Can be if the main admin email is not matching a user, so get the first admin
 		if ( ! $userinfo ) {
-			$user_query = new WP_User_Query( array( 'role' => 'administrator' ) );
+			$user_query = new WP_User_Query( array( 'role' => 'administrator', 'number' => 1 ) );
 			$users      = $user_query->get_results(); // array of WP_User objects, like get_users
 			$userinfo   = $users[0];
 		}

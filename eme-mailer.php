0c82324b156912a74845ef28e04dbbefae012dd8
Franky Van Liedekerke <franky@e-dynamics.be>
2024-07-20 21:26:13 +0200

    fix php warning

--- eme-mailer.php
+++ eme-mailer.php
@@ -2181,7 +2181,7 @@ function eme_emails_page() {
 					$member_ids = explode( ',', $conditions['eme_eventmail_send_members'] );
 					$members    = eme_get_members( $member_ids );
 					foreach ( $members as $member ) {
-						$mymembergroups[ $member['member_id'] ] = eme_format_full_name( $member['firstname'], $member['lastname'], $person['email'] );
+						$mymembergroups[ $member['member_id'] ] = eme_format_full_name( $member['firstname'], $member['lastname'], $member['email'] );
 					}
 				}
 				if ( ! empty( $conditions['eme_eventmail_send_groups'] ) ) {

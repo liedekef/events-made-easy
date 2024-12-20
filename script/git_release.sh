03e76bd14aaf079ff82703ae52ab72728809fb8d
Franky Van Liedekerke <franky@e-dynamics.be>
2024-12-20 23:38:02 +0100

    update release script

--- script/git_release.sh
+++ script/git_release.sh
@@ -7,29 +7,34 @@ if [ -z "$release" ]; then
        exit
 fi       
 
+scriptpath=$(realpath "$0")
+scriptdir=$(dirname $scriptpath)
+basedir=$(dirname $scriptdir)
+cd $basedir
+
 # first create a minimal zip of the previous release
-cd /home/liedekef/wordpress/git
+cd $basedir/..
 #zip -r events-made-easy-minimal.zip events-made-easy -x '*.git*' 'events-made-easy/langs/*.po' 'events-made-easy/langs/pot_gen*' 'events-made-easy/langs/*.pot' 'events-made-easy/langs/gettextize.sh*' 'events-made-easy/screenshot*' -x 'events-made-easy/dist*' -x 'events-made-easy/changelog.txt' -x 'events-made-easy/script*' -x 'events-made-easy/dompdf*' -x 'events-made-easy/payment_gateways*'
 #mv events-made-easy-minimal.zip events-made-easy/dist
 
 
 # If wanted, automate language file updates
-cd /home/liedekef/wordpress/git/events-made-easy/langs
+cd $basedir/langs
 ./gettextize.sh
 
 # now update the release version
-cd /home/liedekef/wordpress/git/events-made-easy
+cd $basedir
 sed -i "s/$old_release/$release/" events-manager.php
 sed -i "s/Stable tag: $old_release/Stable tag: $release/" readme.txt
 
 # now create a zip of the new release
-cd /home/liedekef/wordpress/git
+cd $basedir/..
 # some payment gateways (sumup) look at composer.json for their version info, so don't exclude that
 zip -r events-made-easy.zip events-made-easy -x '*.git*' 'events-made-easy/langs/*.po' 'events-made-easy/langs/pot_gen*' 'events-made-easy/langs/*.pot' 'events-made-easy/langs/gettextize.sh*' 'events-made-easy/screenshot*' -x 'events-made-easy/dist*' -x 'events-made-easy/changelog.txt' -x 'events-made-easy/script*' -x 'events-made-easy/payment_gateways/*/composer.lock'
-mv events-made-easy.zip events-made-easy/dist
+mv events-made-easy.zip $basedir/dist
 
 # move 
-cd /home/liedekef/wordpress/git/events-made-easy
+cd $basedir
 git commit -m "release $release" -a
 git push
 gh release create "v${release}" --generate-notes ./dist/*.zip

69944898902212ebe013e9c55474e0bff4a41ca0
Franky Van Liedekerke <franky@e-dynamics.be>
2025-05-16 00:28:10 +0200

    remove old commented code

--- js/eme_show_maps.js
+++ js/eme_show_maps.js
@@ -1,11 +1,3 @@
-
-//jQuery(document).ready(function() {
-// we use the listeners to resize stuff also for responsive themes
-// but resize is triggered on scrolldown/up too, so we don't use it
-// google.maps.event.addDomListener(window, 'resize', resizeGMap);
-//	google.maps.event.addDomListener(window, 'load', loadGMap);
-//});
-
 jQuery(document).ready(function($) {
     // first the global map (if present)
     let divs = document.getElementsByTagName('div');

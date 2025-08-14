6ed4b51fcee51a2a6e28bd957fe94ef1243df87e
Franky Van Liedekerke <franky@e-dynamics.be>
2025-08-14 10:31:16 +0200

    fdatepicker update

--- js/fdatepicker/js/i18n/fdatepicker.nl-BE.js
+++ js/fdatepicker/js/i18n/fdatepicker.nl-BE.js
@@ -1,14 +1,14 @@
 // datepicker-nl.js
 FDatepicker.setMessages({
-    days: ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'],
-    daysShort: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'],
-    daysMin: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'],
-    months: ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
-    monthsShort: ['Jan', 'Feb', 'Maa', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+    days: [ 'Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag' ],
+    daysShort: [ 'Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za' ],
+    daysMin: [ 'Z', 'M', 'D', 'W', 'D', 'V', 'Z' ],
+    months: [ 'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December' ],
+    monthsShort: [ 'Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec' ],
     today: 'Vandaag',
     clear: 'Wissen',
     close: 'Sluiten',
-    format: 'd/m/Y H:i',
+    format: 'd-m-Y H:i',
     firstDayOfWeek: 1,
     noDatesSelected: 'Geen datums geselecteerd',
     datesSelected: 'Geselecteerde datums ({0}):'
--- js/fdatepicker/js/i18n/fdatepicker.nl.js
+++ js/fdatepicker/js/i18n/fdatepicker.nl.js
@@ -1,10 +1,10 @@
 // datepicker-nl.js
 FDatepicker.setMessages({
-    days: ['Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag'],
-    daysShort: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'],
-    daysMin: ['Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za'],
-    months: ['Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December'],
-    monthsShort: ['Jan', 'Feb', 'Maa', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+    days: [ 'Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag' ],
+    daysShort: [ 'Zo', 'Ma', 'Di', 'Wo', 'Do', 'Vr', 'Za' ],
+    daysMin: [ 'Z', 'M', 'D', 'W', 'D', 'V', 'Z' ],
+    months: [ 'Januari', 'Februari', 'Maart', 'April', 'Mei', 'Juni', 'Juli', 'Augustus', 'September', 'Oktober', 'November', 'December' ],
+    monthsShort: [ 'Jan', 'Feb', 'Mrt', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec' ],
     today: 'Vandaag',
     clear: 'Wissen',
     close: 'Sluiten',

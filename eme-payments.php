73d3baf7fc8e65e9114c1689f80d1aaed0991875
Franky Van Liedekerke <franky@e-dynamics.be>
2025-10-25 22:40:07 +0200

    less restriction for sending payment mail if price is 0

--- eme-payments.php
+++ eme-payments.php
@@ -3852,7 +3852,7 @@ function eme_mark_payment_paid( $payment_id, $is_ipn = 1, $pg = '', $pg_pid = ''
 
             // Send the paid email if the event price is >0, not when the total price to pay is >0, since that can be 0 due to discount
             $booking_event_price = eme_get_booking_event_price( $booking );
-            if ( !$mail_sent && $mailing_paid && ($total_price > 0 || ($total_price==0 && $booking_event_price>0 && !$mailing_approved) ) ) {
+            if ( !$mail_sent && $mailing_paid && ($total_price > 0 || ($total_price==0 && $booking_event_price>0) ) ) {
                 eme_email_booking_action( $booking, 'paidBooking' );
             }
 

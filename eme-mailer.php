b6c18a605bf351eba50c8762b8a9a1a543e76307
Franky Van Liedekerke <franky@e-dynamics.be>
2025-11-19 18:44:06 +0100

    typo

--- eme-mailer.php
+++ eme-mailer.php
@@ -158,14 +158,14 @@ function eme_send_mail( $subject, $body, $receiveremail, $receivername = '', $re
             // it is cleaner than add_filter/remove_filter of wp_mail_content_type ...
             $headers[] = 'Content-type: text/html';
             //add_filter( 'wp_mail_content_type', 'eme_set_wpmail_html_content_type' );
-	} else {
+        } else {
             // use phpmailer to convert html
             require_once ABSPATH . WPINC . '/PHPMailer/PHPMailer.php';
             require_once ABSPATH . WPINC . '/PHPMailer/SMTP.php';
             require_once ABSPATH . WPINC . '/PHPMailer/Exception.php';
             $mail = new PHPMailer\PHPMailer\PHPMailer();
             $body = $mail->html2text( eme_replacelinks( $body ) );
-	}
+        }
 
         // now send it
         if ( ! empty( $mailoptions['toMail'] ) ) {

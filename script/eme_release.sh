12b00f3bc1ca662a908ebef78ec6aeb9f9daab61
Franky Van Liedekerke <franky@e-dynamics.be>
2026-02-07 00:00:55 +0100

    update release script

--- script/eme_release.sh
+++ script/eme_release.sh
@@ -39,7 +39,7 @@ sed -i "s/Stable tag: $old_release/Stable tag: $release/" readme.txt
 # now create a zip of the new release
 cd $basedir/..
 # some payment gateways (sumup) look at composer.json for their version info, so don't exclude that
-zip -r $plugin.zip $plugin -x '*.git*' "$plugin/langs/*.po" "$plugin/langs/pot_gen*" "$plugin/langs/*.pot" "$plugin/langs/gettextize.sh*" "$plugin/dist*" "$plugin/changelog.txt" "$plugin/script*" "$plugin/payment_gateways/*/composer.lock" "$plugin/js/fdatepicker/js/fdatepicker.js" "$plugin/js/ftable/ftable.js" "$plugin/js/jodit/jodit.fat.js" "$plugin/js/sortable/sortable.js" "$plugin/js/leaflet-gesturehandling-1.2.1/leaflet-gesture-handling.js" "$plugin/js/tomselect/tom-select.complete.js"
+zip -r $plugin.zip $plugin -x '*.git*' "$plugin/langs/*.po" "$plugin/langs/pot_gen*" "$plugin/langs/*.pot" "$plugin/langs/gettextize.sh*" "$plugin/dist*" "$plugin/changelog.txt" "$plugin/script*" "$plugin/payment_gateways/*/composer.lock" "$plugin/js/fdatepicker/js/fdatepicker.js" "$plugin/js/ftable/ftable.js" "$plugin/js/jodit/jodit.fat.js" "$plugin/js/sortable/sortable.js" "$plugin/js/leaflet-gesturehandling*/leaflet-gesture-handling.js" "$plugin/js/tomselect/tom-select.complete.js" "$plugin/js/jodit/update-jodit.sh"
 mv $plugin.zip $basedir/dist
 
 # move 

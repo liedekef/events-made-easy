1945112903737e4245941cfc18ea024f668ed1bf
Franky Van Liedekerke <franky@e-dynamics.be>
2026-07-02 15:47:08 +0200

    a group is allowed to be empty

--- includes/eme-people.php
+++ includes/eme-people.php
@@ -2808,7 +2808,7 @@ function eme_group_edit_layout( $group_id = 0, $message = '', $group_type = 'sta
         </tr>
         <tr>
         <td><label for="People"><?php esc_html_e( 'People', 'events-made-easy' ); ?></label></td>
-        <td><?php echo eme_ui_multiselect( $grouppersons, 'persons', $mygroups, 5, '', 1, 'eme_snapselect_people_class' ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- trusted HTML from eme_ui_multiselect() ?></td>
+        <td><?php echo eme_ui_multiselect( $grouppersons, 'persons', $mygroups, 5, '', 0, 'eme_snapselect_people_class' ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- trusted HTML from eme_ui_multiselect() ?></td>
         </tr>
 <?php
             } elseif ( $group['type'] == 'dynamic_people' ) {

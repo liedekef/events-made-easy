02c78d2421bb605c8e3669648f6f30f2caf7598b
Franky Van Liedekerke <franky@e-dynamics.be>
2026-08-21 17:49:29 +0200

    uniform location of filter button

--- includes/eme-events.php
+++ includes/eme-events.php
@@ -6224,7 +6224,6 @@ function eme_events_table( $message = '', $active_tab = '' ) {
         <input type="search" name="search_name" id="events_search_name" placeholder="<?php esc_attr_e( 'Event name', 'events-made-easy' ); ?>" class='eme_searchfilter'>
         <input id="events_search_start_date" type="text" name="events_search_start_date" value="" readonly="readonly" placeholder="<?php esc_attr_e( 'Filter on start date', 'events-made-easy' ); ?>" size=15 data-date='' class='eme_formfield_fdate eme_searchfilter'>
         <input id="events_search_end_date" type="text" name="events_search_end_date" value="" readonly="readonly" placeholder="<?php esc_attr_e( 'Filter on end date', 'events-made-easy' ); ?>" size=15 data-date='' class='eme_formfield_fdate eme_searchfilter'>
-        <button id="EventsLoadRecordsButton" class="button-primary action"><?php esc_html_e( 'Filter events', 'events-made-easy' ); ?></button>
         <button type="button" class="eme-filters-toggle" data-showhide="events_extra_searchfields"><span class="eme-filters-toggle-icon">&#9660;</span> <?php esc_html_e( 'Extra filters', 'events-made-easy' ); ?></button>
         <div id="events_extra_searchfields" class='eme-filters-panel'>
 <?php
@@ -6244,6 +6243,7 @@ function eme_events_table( $message = '', $active_tab = '' ) {
         </div>
 <?php
 ?>
+        <button id="EventsLoadRecordsButton" class="button-primary action"><?php esc_html_e( 'Filter events', 'events-made-easy' ); ?></button>
         </form>
         </div>
 <?php
